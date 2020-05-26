85f84e1afcd5bb24bccfb3f8eee6443a9f3e0b6b
B+Tree <1831518139@qq.com>
2020-05-26 09:30:33 +0800

    第二次修改demo01.py文件

--- demo01.py
+++ demo01.py
@@ -3,12 +3,15 @@
 """
 import time
 
-
+s_num = 0
 num = 1
-while num < 10:
-    print(num)
+while num <= 10:
+    """求1-10之间的整数和"""
+    s_num += num
     num += 1
     time.sleep(1)
+
+print(f"1-10z之间的整数和是{s_num}")
 
 
 
